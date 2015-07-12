4a489957a14e2fd0fc532053a8f74a78df6822d4
Zachary Erskine <bboplifa@gmail.com>
2015-07-12 16:54:40 +0300

    add more dopeness

--- CssTests.php
+++ CssTests.php
@@ -25,45 +25,94 @@ class CssTests extends PHPUnit_Framework_TestCase
     ];
     protected $elementsToTest = [
         'top_products' => [
-            'css_selector' => '.product-link-button.big',
-            'attributes' => [
-                'width' => '330px',
-                'height' => '38px',
-                'font-size' => '20px'
+            'elements' => [
+                'css_selector' => '.product-link-button.big',
+                'attributes' => [
+                    'hover' => [
+                        'width' => '330px',
+                        'height' => '38px',
+                        'font-size' => '20px'
+                    ],
+                    'non_hover' => [
+                        'width' => '330px',
+                        'height' => '38px',
+                        'font-size' => '20px'
+                    ],
+                ]
             ]
         ],
         'chart' => [
-            'css_selector' => '.chart-table .product-link .link-type-btn',
-            'attributes' => [
-                'width' => '113px',
-                'height' => '40px',
-                'font-size' => '18px'
-            ]
-        ],
-        'feature_comparison' => [
-            'css_selector' => '.link-type-btn.small',
-            'attributes' => [
-                'width' => '113px',
-                'height' => '30px',
-                'font-size' => '18px'
-            ]
-        ],
-        'editors_review' => [
-            'css_selector' => 'a.product-link-button.big',
-            'attributes' => [
-                'width' => '330px',
-                'height' => '38px',
-                'font-size' => '20px'
-            ]
-        ],
-        'article' => [
-            'css_selector' => 'a.product-link-button.big',
-            'attributes' => [
-                'width' => '330px',
-                'height' => '38px',
-                'font-size' => '20px'
+            'elements' => [
+                'css_selector' => '.chart-table .product-link .link-type-btn',
+                'attributes' => [
+                    'hover' => [
+                        'width' => '113px',
+                        'height' => '40px',
+                        'font-size' => '18px'
+                    ],
+                    'non_hover' => [
+                        'width' => '113px',
+                        'height' => '40px',
+                        'font-size' => '18px'
+                    ],
+                ]
             ]
         ]
+        ,
+        'feature_comparison' => [
+            'elements' => [
+                'css_selector' => '.link-type-btn.small',
+                'attributes' => [
+                    'hover' => [
+                        'width' => '113px',
+                        'height' => '30px',
+                        'font-size' => '18px'
+                    ],
+                    'non_hover' => [
+                        'width' => '113px',
+                        'height' => '30px',
+                        'font-size' => '18px'
+                    ],
+                ]
+            ]
+        ]
+        ,
+        'editors_review' => [
+            'elements' => [
+                'css_selector' => 'a.product-link-button.big',
+                'attributes' => [
+                    'hover' => [
+                        'width' => '330px',
+                        'height' => '38px',
+                        'font-size' => '20px'
+                    ],
+                    'non_hover' => [
+                        'width' => '330px',
+                        'height' => '38px',
+                        'font-size' => '20px'
+                    ],
+                ]
+            ]
+
+        ],
+        'article' => [
+            'elements' => [
+                'css_selector' => 'a.product-link-button.big',
+                'attributes' => [
+                    'hover' => [
+                        'width' => '330px',
+                        'height' => '38px',
+                        'font-size' => '20px'
+                    ],
+                    'non_hover' => [
+                        'width' => '330px',
+                        'height' => '38px',
+                        'font-size' => '20px'
+                    ],
+                ]
+            ]
+        ]
+
     ];
 
     public function setUp()
@@ -89,26 +138,27 @@ class CssTests extends PHPUnit_Framework_TestCase
                 $this->webDriver->get($url);
 
                 // checking that page title contains word 'GitHub'
-                $element = $this->elementsToTest[$componentName];
+                $elementSet = $this->elementsToTest[$componentName];
 
                 try {
-                    foreach(['',':hover'] as $hoverState) {
-                        $selector = $element['css_selector'];
+                    foreach ($elementSet as $actionStateAttributes) {
+                        $selector = $actionStateAttributes['css_selector'];
                         $htmlElement = $this->webDriver->findElement(WebDriverBy::cssSelector($selector));
-                        if($hoverState){
 
-                            $this->webDriver->getMouse()->mouseMove( $htmlElement->getCoordinates() );
-                        }
                         $cssValue = $cssExpectedValue = '';
-                        foreach ($element['attributes'] as $attributeType => $attributeValue) {
-
-                            $cssValue = $htmlElement->getCSSValue($attributeType);
-                            $cssExpectedValue = $attributeValue;
-                            $this->assertEquals($cssValue, $cssExpectedValue);
+                        foreach ($actionStateAttributes['attributes'] as $actionState => $attributes) {
+                            foreach ($attributes as $attributeType => $attributeValue) {
+                                if ($actionState === 'hover') {
+                                    $this->webDriver->getMouse()->mouseMove($htmlElement->getCoordinates());
+                                }
+                                $cssValue = $htmlElement->getCSSValue($attributeType);
+                                $cssExpectedValue = $attributeValue;
+                                $this->assertEquals($cssValue, $cssExpectedValue);
+                            }
                         }
                     }
-                } catch (\Exception $e){
-                    print_r(array($e->getMessage(),$selector, $url,$cssValue,$cssExpectedValue));
+                } catch (\Exception $e) {
+                    print_r(array($e->getMessage(), $selector, $url, $cssValue, $cssExpectedValue));
                 }
             }
 
